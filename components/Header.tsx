b68e5337a41d7ab7de30262ae0a045f295680c2b
kumurbek <74360101+kumbey@users.noreply.github.com>
2022-09-26 17:12:10 +0800

    fin

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 type Props = {};
 
@@ -23,43 +24,55 @@ function Header({}: Props) {
         <SocialIcon
           url="https://www.facebook.com/kumurbek"
           fgColor="gray"
+          target={"_blank"}
           bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.twitter.com/kumurbek"
+          fgColor="gray"
+          bgColor="transparent"
+          target={"_blank"}
         />
         <SocialIcon
           url="https://github.com/kumbey"
           fgColor="gray"
+          target={"_blank"}
           bgColor="transparent"
         />
         <SocialIcon
           url="https://www.linkedin.com/in/khumarbyek-beybit/"
           fgColor="gray"
+          target={"_blank"}
           bgColor="transparent"
         />
       </motion.div>
-      <motion.div
-        className="flex items-center text-gray-300 cursor-pointer"
-        initial={{
-          x: 500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{ duration: 1.5 }}
-      >
-        <SocialIcon
-          className="cursor-pointer"
-          network="email"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <p className="uppercase hidden  md:inline-flex text-sm text-gray-400">
-          get in touch
-        </p>
-      </motion.div>
+
+      <Link href={"#contact"}>
+        <motion.div
+          className="flex items-center text-gray-300 cursor-pointer"
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{ duration: 1.5 }}
+        >
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden  md:inline-flex text-sm text-gray-400">
+            get in touch
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Header from "../components/Header";
@@ -10,7 +11,7 @@ import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Kumurbek's portfolio</title>
       </Head>
@@ -34,6 +35,26 @@ const Home: NextPage = () => {
       <section id="contact" className="snap-start">
         <Contact />
       </section>
+      <Link href={"#hero"}>
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#F7AB0A"
+              className="w-6 h-6 filter grayscale hover:grayscale-0 cursor-pointer"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5"
+              />
+            </svg>
+          </div>
+        </footer>
+      </Link>
       {/* 
     Hero
 
